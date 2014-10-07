3d7900ba683d34ab3af3c2098d2893bea6661296
Zewei Chu <jasonchuzewei@gmail.com>
2014-10-07 20:59:01 +0800

    FIX
    FIX

--- ObjectAlgebras/src/trees/StmAlg.java
+++ ObjectAlgebras/src/trees/StmAlg.java
@@ -4,15 +4,15 @@ import java.util.List;
 import com.zewei.annotation.processor.Algebra;
 
 @Algebra
-public interface StmAlg<Stm, Exp, Typ>{
+public interface StmAlg<Stm, EExp, Typ>{
 	Stm SDecl(Typ t, String v);
-	Stm SAss(String v, Exp e);
+	Stm SAss(String v, EExp e);
 	Stm SBlock(List<Stm> ls);
-	Stm SReturn(Exp e);
-	Exp EStm(Stm s);
-	Exp EAdd(Exp exp1, Exp exp2);
-	Exp EVar(String v);
-	Exp EInt(int i);
+	Stm SReturn(EExp e);
+	EExp EStm(Stm s);
+	EExp EAdd(EExp exp1, EExp exp2);
+	EExp EVar(String v);
+	EExp EInt(int i);
 	Typ TInt();
 	Typ TFloat();
 }
