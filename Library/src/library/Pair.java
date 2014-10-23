1821bdd6477094370ff950b6e74f0677a59dd646
Tijs van der Storm <storm@cwi.nl>
2014-10-23 16:04:34 +0200

    Equals, hashcode and tostring.

--- Library/src/library/Pair.java
+++ Library/src/library/Pair.java
@@ -6,4 +6,24 @@ public class Pair<A, B> {
 	public Pair(A a, B b) { this.a = a; this.b = b; }
 	public A a() { return a; }
 	public B b() { return b; }
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Pair)) {
+			return false;
+		}
+		Pair<A, B> p = (Pair<A,B>)obj;
+		return a.equals(p.a()) && b.equals(p.b());
+	}
+	
+	@Override
+	public int hashCode() {
+		return a.hashCode() + b.hashCode();
+	}
+	
+	@Override
+	public String toString() {
+		return "<" + a + ", " + b + ">";
+	}
 }
