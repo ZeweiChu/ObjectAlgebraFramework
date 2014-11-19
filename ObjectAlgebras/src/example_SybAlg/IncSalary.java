eae4d73011a35151b0097dd1832aaf906d8ba7e9
Tijs van der Storm <storm@cwi.nl>
2014-11-19 15:49:05 +0100

    Modified IncSalary to conform to interfacing trafos.

--- ObjectAlgebras/src/example_SybAlg/IncSalary.java
+++ ObjectAlgebras/src/example_SybAlg/IncSalary.java
@@ -4,8 +4,10 @@ import transform.SybAlgTransform;
 import trees.SybAlg;
 
 //BEGIN_TRANSFORM_WITH_OAFRAMEWORK
-public class IncSalary extends SybAlgTransform<Float, Float, Float, Float, Float, Float> {
-	public IncSalary(SybAlg<Float, Float, Float, Float, Float, Float> alg) {super(alg);}
+public class IncSalary implements SybAlgTransform<Float, Float, Float, Float, Float, Float> {
+	private SybAlg<Float, Float, Float, Float, Float, Float> alg;
+	public SybAlg<Float,Float,Float,Float,Float,Float> sybAlg() {return alg;};
+	public IncSalary(SybAlg<Float, Float, Float, Float, Float, Float> alg) {this.alg=alg;}
 	public Float S(float salary) {return alg.S(1.1f * salary);}
 }
 //END_TRANSFORM_WITH_OAFRAMEWORK
