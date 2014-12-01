d0dcfcd0e8941bb552b4e047cd7e4cc4fbb39466
Zewei Chu <jasonchuzewei@gmail.com>
2014-12-01 14:41:07 +0800

    related work

--- ObjectAlgebras/src/deps1/DepGraph.java
+++ ObjectAlgebras/src/deps1/DepGraph.java
@@ -10,21 +10,16 @@ import trees.StatAlg;
 
 //BEGIN_DEPS1
 public interface DepGraph extends ExpAlg<Set<String>>, StatAlg<Set<String>, Set<Pair<String,String>>> {
-	@Override
 	default Set<String> Var(String p0) {return Collections.singleton(p0);}
-	@Override
 	default Set<String> Lit(int i){return Collections.emptySet();}
-	@Override
 	default Set<String> Add(Set<String> e1, Set<String> e2){
 		Set<String> deps = new HashSet<>(e1);
 		deps.addAll(e2);
 		return deps;}
-	@Override
 	default Set<Pair<String, String>> Assign(String p0,Set<String> p1) {
 		Set<Pair<String,String>> deps = new HashSet<>();
 		for (String x: p1) {deps.add(new Pair<>(p0, x));}
 		return deps;}
-	@Override
 	default Set<Pair<String, String>> Seq(Set<Pair<String, String>> s1, Set<Pair<String, String>> s2){
 		Set<Pair<String, String>> deps = new HashSet<>(s1);
 		deps.addAll(s2);
