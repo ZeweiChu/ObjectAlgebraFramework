61c709efa6df8f676f6c8a62e3908ffda334cd06
Zewei Chu <jasonchuzewei@gmail.com>
2014-12-08 09:42:16 +0800

    Section 2

--- ObjectAlgebras/src/example_SybAlg/IncSalary.java
+++ ObjectAlgebras/src/example_SybAlg/IncSalary.java
@@ -4,10 +4,10 @@ import transform.SybAlgTransform;
 import trees.SybAlg;
 
 //BEGIN_TRANSFORM_WITH_OAFRAMEWORK
-public class IncSalary implements SybAlgTransform<Float, Float, Float, Float, Float, Float> {
-	private SybAlg<Float, Float, Float, Float, Float, Float> alg;
-	public SybAlg<Float,Float,Float,Float,Float,Float> sybAlg() {return alg;}
-	public IncSalary(SybAlg<Float, Float, Float, Float, Float, Float> alg) {this.alg=alg;}
-	public Float S(float salary) {return alg.S(1.1f * salary);}
+public class IncSalary<Company, Dept, SubUnit, Employee, Person, Salary> implements SybAlgTransform<Company, Dept, SubUnit, Employee, Person, Salary> {
+	private SybAlg<Company, Dept, SubUnit, Employee, Person, Salary> alg;
+	public SybAlg<Company, Dept, SubUnit, Employee, Person, Salary> sybAlg() {return alg;}
+	public IncSalary(SybAlg<Company, Dept, SubUnit, Employee, Person, Salary> alg) {this.alg=alg;}
+	public Salary S(float salary) {return alg.S(1.1f * salary);}
 }
 //END_TRANSFORM_WITH_OAFRAMEWORK
