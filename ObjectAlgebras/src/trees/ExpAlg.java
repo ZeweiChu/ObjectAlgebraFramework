d9eb660cb247bb645fe6fbebf3c87d28499f2d10
Zewei Chu <jasonchuzewei@gmail.com>
2014-12-11 09:01:04 +0800

    update

--- ObjectAlgebras/src/trees/ExpAlg.java
+++ ObjectAlgebras/src/trees/ExpAlg.java
@@ -2,11 +2,11 @@ package trees;
 
 import com.zewei.annotation.processor.Algebra;
 
-//BEGIN_EXPALG
+//BEGIN_EXP_ALG
 @Algebra
 public interface ExpAlg<Exp> {
 	Exp Var(String s);
 	Exp Lit(int i);
 	Exp Add(Exp e1, Exp e2);
 }
-//END_EXPALG
+//END_EXP_ALG
