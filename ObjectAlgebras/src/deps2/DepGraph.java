5260c85ca1f56b07162b1f577d45ad5ba3045094
Tijs van der Storm <storm@cwi.nl>
2014-12-11 11:57:12 +0100

    Improvements.

--- ObjectAlgebras/src/deps2/DepGraph.java
+++ ObjectAlgebras/src/deps2/DepGraph.java
@@ -1,21 +1,18 @@
 package deps2;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import static java.util.Collections.singleton;
 
+import java.util.Set;
+import static java.util.stream.Collectors.toSet;
 import library.Pair;
 import query.G_ExpAlgQuery;
 import query.G_StatAlgQuery;
 
 //BEGIN_DEPS2
 public interface DepGraph extends G_ExpAlgQuery<Set<String>>, G_StatAlgQuery<Set<String>, Set<Pair<String,String>>> {
-	default Set<String> Var(String p0) {return Collections.singleton(p0);}
-	default Set<Pair<String, String>> Assign(String p0,Set<String> p1) {
-		Set<Pair<String,String>> deps = new HashSet<>();
-		for (String x: p1) {
-			deps.add(new Pair<>(p0, x));
-		}
-		return deps;}
+	default Set<String> Var(String p0) { return singleton(p0); }
+	default Set<Pair<String, String>> Assign(String p0, Set<String> p1) {
+		return p1.stream().map((x) -> new Pair<>(p0, x)).collect(toSet());
+	}
 }
 //END_DEPS2
---
+++ ObjectAlgebras/src/deps2/G_StatAlgQuery.java
@@ -0,0 +1,12 @@
+package deps2;
+
+import library.Monoid;
+import trees.StatAlg;
+
+//BEGIN_GSTAT_QUERY
+public interface G_StatAlgQuery<Exp, Stat> extends StatAlg<Exp, Stat> {
+	Monoid<Exp> mExp(); Monoid<Stat> mStat();
+	default Stat Assign(String p0, Exp p1) { return mStat().empty(); }
+	default Stat Seq(Stat p0, Stat p1) { return mStat().join(p0, p1); }
+}
+//END_GSTAT_QUERY
