585f4ea0ad6a17bdd26a396f8b9ca4793a476efc
Tijs van der Storm <storm@cwi.nl>
2014-12-12 10:54:58 +0100

    Tweaks.

--- ObjectAlgebras/src/deps2/DepGraph.java
+++ ObjectAlgebras/src/deps2/DepGraph.java
@@ -10,9 +10,9 @@ import query.G_StatAlgQuery;
 
 //BEGIN_DEPS2
 public interface DepGraph extends G_ExpAlgQuery<Set<String>>, G_StatAlgQuery<Set<String>, Set<Pair<String,String>>> {
-	default Set<String> Var(String p0) { return singleton(p0); }
-	default Set<Pair<String, String>> Assign(String p0, Set<String> p1) {
-		return p1.stream().map((x) -> new Pair<>(p0, x)).collect(toSet());
+	default Set<String> Var(String x) { return singleton(x); }
+	default Set<Pair<String, String>> Assign(String x, Set<String> e) {
+		return e.stream().map(y -> new Pair<>(x, y)).collect(toSet());
 	}
 }
 //END_DEPS2
