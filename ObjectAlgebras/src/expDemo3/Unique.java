2dea4192634c0d1c62eb267fab3f9fc4ab9171c6
Tijs van der Storm <storm@cwi.nl>
2014-12-18 13:56:43 +0100

    Small paragraph on extensible trafos.

--- ObjectAlgebras/src/expDemo3/Unique.java
+++ ObjectAlgebras/src/expDemo3/Unique.java
@@ -1,8 +1,8 @@
 package expDemo3;
 
 //BEGIN_UNIQUEVARS
-public interface Unique<E> extends ExpAlgTransform<E> {
+interface Unique<E> extends ExpAlgTransform<E> {
 	int nextInt();
-	default E Var(String s) { return expAlg().Var(s + "#" + nextInt()); }
+	default E Var(String s) { return expAlg().Var(s + nextInt()); }
 }
 //END_UNIQUEVARS
--- ObjectAlgebras/src/expDemo3/UniqueWithLambda.java
+++ ObjectAlgebras/src/expDemo3/UniqueWithLambda.java
@@ -1,8 +1,8 @@
 package expDemo3;
 
 //BEGIN_EXTEND_UNIQUEVARS
-public interface UniqueWithLambda<E> extends Unique<E>, LamAlgTransform<E> {
-	default E Lam(String x, E e) { return lamAlg().Lam(x + "#" + nextInt(), e); }
+interface UniqueWithLambda<E> extends Unique<E>, LamAlgTransform<E> {
+	default E Lam(String x, E e) { return lamAlg().Lam(x + nextInt(), e); }
 }
 //END_EXTEND_UNIQUEVARS
 
