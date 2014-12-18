a9a284639c05cef6e64851b188c337d47db87016
Tijs van der Storm <storm@cwi.nl>
2014-12-18 15:54:54 +0100

    Made 101companies consistent.

--- ObjectAlgebras/src/trees/SybAlg.java
+++ ObjectAlgebras/src/trees/SybAlg.java
@@ -6,11 +6,11 @@ import com.zewei.annotation.processor.Algebra;
 
 //BEGIN_SYB_TREE
 @Algebra
-public interface SybAlg<Company, Dept, SubUnit, Employee, Person, Salary>{
+public interface SybAlg<Company, Dept, Unit, Employee, Person, Salary>{
   public Company C(List<Dept> depts);
-  public Dept D(String name, Employee manager, List<SubUnit> subUnits);
-  public SubUnit PU(Employee employee);
-  public SubUnit DU(Dept dept);
+  public Dept D(String name, Employee manager, List<Unit> units);
+  public Unit PU(Employee employee);
+  public Unit DU(Dept dept);
   public Employee E(Person p, Salary s);
   public Person P(String name, String address);
   public Salary S(float salary);
