e573f16a3d0dea6881e07229ad731e9515212fa3
Tijs van der Storm <storm@cwi.nl>
2014-12-18 15:35:28 +0100

    Spaces.

--- ObjectAlgebras/src/expDemo3/ExpAlgQuery.java
+++ ObjectAlgebras/src/expDemo3/ExpAlgQuery.java
@@ -3,8 +3,8 @@ package expDemo3;
 //BEGIN_GENERIC_QUERY
 interface ExpAlgQuery<Exp> extends ExpAlg<Exp> {
 	Monoid<Exp> m();
-	default Exp Var(String s) {return m().empty();} 
-	default Exp Lit(int i) {return m().empty();} 
-	default Exp Add(Exp e1, Exp e2) {return m().join(e1, e2);}
+	default Exp Var(String s) { return m().empty(); } 
+	default Exp Lit(int i) { return m().empty(); } 
+	default Exp Add(Exp e1, Exp e2) { return m().join(e1, e2); }
 }
 //END_GENERIC_QUERY
--- ObjectAlgebras/src/expDemo3/FreeVars.java
+++ ObjectAlgebras/src/expDemo3/FreeVars.java
@@ -6,7 +6,7 @@ import java.util.Set;
 
 //BEGIN_FREEVARS_WITH_MONOID
 interface FreeVars extends ExpAlgQuery<Set<String>> {
-	default Monoid<Set<String>> m() {return new SetMonoid<String>();}
-	default Set<String> Var(String s) {return singleton(s);}
+	default Monoid<Set<String>> m() { return new SetMonoid<String>(); }
+	default Set<String> Var(String s) { return singleton(s); }
 }
 //END_FREEVARS_WITH_MONOID
