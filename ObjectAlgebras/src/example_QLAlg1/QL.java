d7783c6bd5059226d154a076f992b476e4ccc35b
hyzhang <zhanghaoyuan00@gmail.com>
2015-03-12 16:00:49 +0800

    Revising code.

--- ObjectAlgebras/src/example_QLAlg1/QL.java
+++ ObjectAlgebras/src/example_QLAlg1/QL.java
@@ -1,22 +1,25 @@
 package example_QLAlg1;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 //BEGIN_OO_APPROACH
-class QL {
-	Set<String> usedVars() { return Collections.emptySet(); }
-	void renaming() {}
+import static java.util.Collections.emptySet;
+import static java.util.Collections.singleton;
+import static java.util.stream.Stream.concat;
+import static java.util.stream.Collectors.toSet;
+import static java.util.stream.Collectors.toList;
+
+abstract class Exp {
+	Set<String> usedVars() { return emptySet(); }
+	abstract Exp renaming();
 }
 
-class Exp extends QL {}
+abstract class Stmt {
+	Set<String> usedVars() { return emptySet(); }
+	abstract Stmt renaming();
+}
 
-class Stmt extends QL {}
-
-class Form extends QL {
+class Form {
 	private String id;
 	private List<Stmt> statements;
 	Form(String id, List<Stmt> statements) {
@@ -24,14 +27,10 @@ class Form extends QL {
 		this.statements = new ArrayList<Stmt>(statements);
 	}
 	Set<String> usedVars() {
-		Set<String> res = new HashSet<String>();
-		for (Stmt s : statements)
-			res.addAll(s.usedVars());
-		return res;
+		return statements.stream().map(x -> x.usedVars()).reduce(emptySet(), (x, y) -> concat(x.stream(), y.stream()).collect(toSet()));
 	}
-	void renaming() {
-		for (Stmt s : statements)
-			s.renaming();
+	Form renaming() {
+		return new Form(id, statements.stream().map(s -> s.renaming()).collect(toList()));
 	}
 }
 
@@ -40,13 +39,10 @@ class Iff extends Stmt {
 	private Stmt then;
 	Iff(Exp cond, Stmt then) { this.cond = cond; this.then = then; }
 	Set<String> usedVars() {
-		Set<String> res = new HashSet<String>(cond.usedVars());
-		res.addAll(then.usedVars());
-		return res;
+		return concat(cond.usedVars().stream(), then.usedVars().stream()).collect(toSet());
 	}
-	void renaming() {
-		cond.renaming();
-		then.renaming();
+	Iff renaming() {
+		return new Iff(cond.renaming(), then.renaming());
 	}
 }
 
@@ -57,32 +53,30 @@ class Question extends Stmt {
 		this.label = label;
 		this.type = type;
 	}
-	void renaming() { id += "_"; }
+	Question renaming() { return new Question(id + "_", label, type); }
 }
 
 class Lit extends Exp {
 	private int x;
 	Lit(int x) { this.x = x; }
+	Lit renaming() { return new Lit(x); }
 }
 
 class Var extends Exp {
 	private String varName;
 	Var(String varName) { this.varName = varName; }
-	Set<String> usedVars() { return Collections.singleton(varName); }
-	void renaming() { varName += "_"; }
+	Set<String> usedVars() { return singleton(varName); }
+	Var renaming() { return new Var(varName + "_"); }
 }
 
 class Geq extends Exp {
 	private Exp lhs, rhs;
 	Geq(Exp lhs, Exp rhs) { this.lhs = lhs; this.rhs = rhs; }
 	Set<String> usedVars() {
-		Set<String> res = new HashSet<String>(lhs.usedVars());
-		res.addAll(rhs.usedVars());
-		return res;
+		return concat(lhs.usedVars().stream(), rhs.usedVars().stream()).collect(toSet());
 	}
-	void renaming() {
-		lhs.renaming();
-		rhs.renaming();
+	Geq renaming() {
+		return new Geq(lhs.renaming(), rhs.renaming());
 	}
 }
 //END_OO_APPROACH
--- ObjectAlgebras/src/example_QLAlg2/UsedVars.java
+++ ObjectAlgebras/src/example_QLAlg2/UsedVars.java
@@ -1,37 +1,26 @@
 package example_QLAlg2;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import trees.QLAlg;
 
 //BEGIN_QL_QUERY_ALG
-class UsedVars implements QLAlg<Set<String>, Set<String>, Set<String>>{
+import static java.util.Collections.emptySet;
+import static java.util.Collections.singleton;
+import static java.util.stream.Stream.concat;
+import static java.util.stream.Collectors.toSet;
+
+class UsedVars implements QLAlg<Set<String>, Set<String>, Set<String>> {
 	public Set<String> form(String id, List<Set<String>> statements) {
-		Set<String> res = new HashSet<String>();
-		for (Set<String> s : statements)
-			res.addAll(s);
-		return res;
+		return statements.stream().reduce(emptySet(), (x, y) -> concat(x.stream(), y.stream()).collect(toSet()));
 	}
 	public Set<String> iff(Set<String> cond, Set<String> then) {
-		Set<String> res = new HashSet<String>(cond);
-		res.addAll(then);
-		return res;
+		return concat(cond.stream(), then.stream()).collect(toSet());
 	}
-	public Set<String> question(String id, String label, String type) {
-		return Collections.emptySet();
-	}
-	public Set<String> lit(int x) {
-		return Collections.emptySet();
-	}
-	public Set<String> var(String varName) {
-		return Collections.singleton(varName);
-	}
+	public Set<String> question(String id, String label, String type) { return emptySet(); }
+	public Set<String> lit(int x) { return emptySet(); }
+	public Set<String> var(String varName) { return singleton(varName); }
 	public Set<String> geq(Set<String> lhs, Set<String> rhs) {
-		Set<String> res = new HashSet<String>(lhs);
-		res.addAll(rhs);
-		return res;
+		return concat(lhs.stream(), rhs.stream()).collect(toSet());
 	}
 }
 //END_QL_QUERY_ALG
