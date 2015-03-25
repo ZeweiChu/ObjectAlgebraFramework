62c9501efb973e4d247378b30bec3be464099e1f
hyzhang <zhanghaoyuan00@gmail.com>
2015-03-25 13:07:18 +0800

    Revised S5 code; figure captions S1-5

--- ObjectAlgebras/src/deps2/TestDepGraph.java
+++ ObjectAlgebras/src/deps2/TestDepGraph.java
@@ -1,18 +1,13 @@
 package deps2;
 
-import java.util.Set;
-
 import trees.ExpAlg;
 import trees.StatAlg;
-import library.Monoid;
-import library.Pair;
-import monoid.SetMonoid;
 
 public class TestDepGraph {
 
 //BEGIN_GEN_DEPGRAPH
 <E, S, A extends ExpAlg<E> & StatAlg<E, S>>
-		S makeExp(A alg) {
+		S makeStat(A alg) {
 	return alg.Seq(
 			alg.Assign("x", alg.Add(alg.Var("x"),
 					alg.Lit(3))), 
@@ -23,13 +18,8 @@ public class TestDepGraph {
 
 	void Test() {
 
-
-DepGraph depGraph = new DepGraph() {
-	public Monoid<Set<String>> mExp() { return new SetMonoid<>(); }
-	public Monoid<Set<Pair<String, String>>> mStat() { return new SetMonoid<>(); }
-};
 //BEGIN_CLIENTCODE_DEPGRAPH
-println(makeExp(depGraph));
+println(makeStat(new DepGraph(){}));
 //END_CLIENTCODE_DEPGRAPH
 
 	}
