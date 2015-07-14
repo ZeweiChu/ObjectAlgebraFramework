85040d7aa4bf4500d3933bbb2ba1b61d34b41841
Tijs van der Storm <storm@cwi.nl>
2015-07-14 13:11:17 +0200

    Stuff

--- QL/src/_syb/trafo/DesugarUnless.java
+++ QL/src/_syb/trafo/DesugarUnless.java
@@ -4,7 +4,7 @@ import transform.IExpAlgTransform;
 import transform.IStmtAlgTransform;
 import transform.IUnlessAlgTransform;
 
-public interface DesugarUnless<E, S> extends IUnlessAlgTransform<E, S>, IStmtAlgTransform<E, S>, IExpAlgTransform<E> {
+public interface DesugarUnless<E, S> extends IUnlessAlgTransform<E, S> {
 	@Override 
 	default S unless(E cond, S body) {
 		return iff(not(cond), body);
