c5002dd753e32bef5e402faca4be7dc9e2e836e6
Tijs van der Storm <storm@cwi.nl>
2015-07-16 13:51:44 +0200

    Section 8

--- ObjectAlgebras/src/trees/DoubleAlg.java
+++ ObjectAlgebras/src/trees/DoubleAlg.java
@@ -4,7 +4,7 @@ import com.zewei.annotation.processor.Algebra;
 
 //BEGIN_DOUBLE_TREE
 @Algebra
-public interface DoubleAlg<Exp> {
-	Exp Double(Exp e);
+public interface DoubleAlg<E> {
+	E Double(E e);
 }
 //END_DOUBLE_TREE
